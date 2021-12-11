c34e8975ea83f3ef32caf010adfb9a727c73e35f
Dimitra Maoutsa <maoutsa.d@hotmail.gr>
2021-12-11 23:52:18 +0100

    Create __init__.py

---
+++ DeterministicParticleFlow/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 11 23:49:44 2021
+
+@author: maout
+"""
+
+
+
+
+from __future__ import absolute_import, division, print_function
+from .version import __version__  
+from .DeterministicParticleFlow import *  
