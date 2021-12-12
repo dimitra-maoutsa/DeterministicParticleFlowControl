82186be5df4f228d441865bc05f14dbcb23b187f
Dimitra Maoutsa <maoutsa.d@hotmail.gr>
2021-12-12 02:35:22 +0100

    Create testing1.py

---
+++ DeterministicParticleFlowControl/tests/testing1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 12 02:32:31 2021
+
+@author: maout
+"""
+
+
+import os.path as op
+import DeterministicParticleFlowControl as dpfc
+data_path = op.join(dpfc.__path__[0], 'data')
