9a69f34fea2e92091b7e6fd9b84ab6fa677b1b4b
Dimitra Maoutsa <maoutsa.d@hotmail.gr>
2021-12-19 19:56:08 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -77,9 +77,22 @@ extensions = ['sphinx.ext.autodoc',
              'sphinx_copybutton']
 
 # For napoleon parsing parameters of functions
-napoleon_google_docstring = False
-napoleon_use_param = False
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
 napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
+
 
 # Add any paths that contain templates here, relative to this directory.
 #templates_path = ['_templates']
