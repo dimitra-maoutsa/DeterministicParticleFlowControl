aae0e07907b629258f82167410a198a4ba911671
Dimitra Maoutsa <maoutsa.d@hotmail.gr>
2021-12-19 19:42:01 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -79,7 +79,7 @@ extensions = ['sphinx.ext.autodoc',
 # For napoleon parsing parameters of functions
 napoleon_google_docstring = False
 napoleon_use_param = False
-napoleon_use_ivar = True
+napoleon_use_ivar = False
 
 # Add any paths that contain templates here, relative to this directory.
 #templates_path = ['_templates']
