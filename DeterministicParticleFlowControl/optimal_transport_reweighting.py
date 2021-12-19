2a5f80f4c9c0f1f507e684cfb3251f604f64358c
Dimitra Maoutsa <maoutsa.d@hotmail.gr>
2021-12-19 06:04:46 +0100

    Update optimal_transport_reweighting.py

--- DeterministicParticleFlowControl/optimal_transport_reweighting.py
+++ DeterministicParticleFlowControl/optimal_transport_reweighting.py
@@ -10,35 +10,43 @@ Created on Sun Dec 12 04:14:07 2021
 
 import numpy as np
 from pyemd import emd_with_flow
-from scipy.spatial.distance import pdist,squareform
-def reweight_optimal_transport_multidim(x,w):
-    
+from scipy.spatial.distance import pdist, squareform
+def reweight_optimal_transport_multidim(samples, weights):
+
     """
     Computes deterministic transport map for particle reweighting.
-    Inputs:
+    Parameters:
     ------
-        x: Samples from distribution M x dim , with dim>=2
-        w: weights for each sample M
-    
-    Reweighting particles according to ensemble transform erticle filter algorithm proposed by Reich 2013
-    Employes OT to compute a resampling scheme which minimises the expected distances
-    between the particles before and after the resampling
+        samples: array-like,
+            Samples from distribution M x dim , with dim>=2.
+        weights: array-like,
+            weights for each sample M.
+
+    Returns:
+    ------
+        T: array like,
+            transport map.
+
+    Reweighting particles according to ensemble transform erticle filter
+    algorithm proposed by Reich 2013
+    Employes OT to compute a resampling scheme which minimises the
+    expected distances between the particles before and after the resampling
     CO = X'*X;
     CO = diag(CO)*ones(1,M) -2*CO + ones(M,1)*diag(CO)';
-    
+
     [dist,T] = emd_hat_mex(ww,ones(M,1)/M,CO,-1,3);
     T = T*M;
-    
+
     """
-    X = x
-    M = x.shape[0] ## this should be the number of points
-    
-    CO = squareform(pdist(X, 'euclidean'))
-    b =  np.ones((M,1)) / M  # uniform distribution on samples    
-    
-    dist, T = emd_with_flow(w.reshape(-1,),b.reshape(-1,), CO,-1)
-    
-    T = np.array(T)*M    
-    
-    
+
+    num_samples = samples.shape[0] ## this should be the number of points
+
+    covar = squareform(pdist(samples, 'euclidean'))
+    b = np.ones((num_samples, 1)) / num_samples  # uniform distribution on samples
+
+    _, T = emd_with_flow(weights.reshape(-1, ), b.reshape(-1, ), covar, -1)
+
+    T = np.array(T)*num_samples
+
+
     return T    #%%
