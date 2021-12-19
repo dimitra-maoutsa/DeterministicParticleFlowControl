c7f8582317d4e5ac3d6322927b5f8478995441c6
Dimitra Maoutsa <maoutsa.d@hotmail.gr>
2021-12-19 23:40:19 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -212,7 +212,7 @@ html_logo = '_static/logo.png'
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+#html_favicon = '_static/logo.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -233,8 +233,8 @@ html_static_path = ['_static']
 html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-html_sidebars = {'**': ['localtoc.html', 'searchbox.html']}
-
+#html_sidebars = {'**': ['localtoc.html', 'searchbox.html']}
+html_sidebars = {'**': ['globaltoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html']}
 #html_sidebars = {   '**': [  'localtoc.html',  'navigation.html',  'relations.html', 'searchbox.html'  ]  }
 
 # Additional templates that should be rendered to pages, maps page names to
