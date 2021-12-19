ec3928536aaf0f57024682f754345cc914b583ae
Dimitra Maoutsa <maoutsa.d@hotmail.gr>
2021-12-19 22:26:54 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -95,7 +95,7 @@ napoleon_attr_annotations = True
 
 
 # Add any paths that contain templates here, relative to this directory.
-#templates_path = ['_templates']
+templates_path = ['_templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
