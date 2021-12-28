408702221564971aa90b9370d8d8dc1f3c9f0ecf
Dimitra Maoutsa <maoutsa.d@hotmail.gr>
2021-12-28 03:56:31 +0100

    Update Bridge_2D_on_Limit_Cycle.py

--- notebooks/Stochastic_bridge_on_Limit_Cycle_2D/Bridge_2D_on_Limit_Cycle.py
+++ notebooks/Stochastic_bridge_on_Limit_Cycle_2D/Bridge_2D_on_Limit_Cycle.py
@@ -30,10 +30,10 @@ plt.rcParams["axes.linewidth"] = 2
 
 
 # Drift of the system
-def f(x,t=0):#LC
+def f(x, t=0):#LC
     x0 = -x[1] + x[0]*(1-x[0]**2 -x[1]**2)
     x1 = x[0] + x[1]*(1-x[0]**2 -x[1]**2)
-    return np.array([x0,x1])
+    return np.array([x0, x1])
 
 # noise amplitude
 g = 0.1
@@ -48,14 +48,14 @@ T = 500.
 
 x_0 = np.array([-0., -1.0])
 
-timegridall = np.arange(0,T,dt)
-F = np.zeros((2,timegridall.size))
+timegridall = np.arange(0, T, dt)
+F = np.zeros((2, timegridall.size))
 
 for ti,t in enumerate(timegridall):
-    if ti==0:
-        F[:,0] = x_0
+    if ti == 0:
+        F[:, 0] = x_0
     else:
-        F[:,ti] = F[:,ti-1]+ dt* f(F[:,ti-1])+(g)*np.random.normal(loc=0.0, scale=np.sqrt(dt), size=(2,))
+        F[:, ti] = F[:, ti-1]+ dt* f(F[:, ti-1])+(g)*np.random.normal(loc=0.0, scale=np.sqrt(dt), size=(2, ))
 
 #%%
 # Set initial and terminal conditions for the bridge and
@@ -75,9 +75,9 @@ t1 = timegridall[100]
 # terminal time
 t2 = timegridall[100+steps]
 # initial state
-y1 = F[:,100]
+y1 = F[:, 100]
 # terminal state
-y2 = F[:,100+steps]
+y2 = F[:, 100+steps]
 
 
 ##create object bridg2d that contains the sampled flows
@@ -105,17 +105,17 @@ plt.show()
 #%%
 # Plot of the sampled constrained flow across ach dimension.
 
-plt.figure(figsize=(10,5)),
-plt.subplot(1,2,1)
-plt.plot(bridg2d.timegrid,bridg2d.B[0,:,:].T,'maroon',alpha=0.5)
-plt.plot(bridg2d.timegrid[-1],y2[0],'x', c='silver',markersize=10)
-plt.plot(bridg2d.timegrid[0],y1[0],'.g')
+plt.figure(figsize=(10, 5)),
+plt.subplot(1, 2, 1)
+plt.plot(bridg2d.timegrid,bridg2d.B[0,:,:].T, 'maroon', alpha=0.5)
+plt.plot(bridg2d.timegrid[-1], y2[0], 'x', c='silver', markersize=10)
+plt.plot(bridg2d.timegrid[0], y1[0],'.g')
 #plt.ylim(-2,2)
 
 plt.subplot(1,2,2)
-plt.plot(bridg2d.timegrid,bridg2d.B[1].T,'maroon',alpha=0.5)
-plt.plot(bridg2d.timegrid[-1],y2[1],'x', c='silver',markersize=10)
-plt.plot(bridg2d.timegrid[0],y1[1],'.g')
+plt.plot(bridg2d.timegrid, bridg2d.B[1].T, 'maroon', alpha=0.5)
+plt.plot(bridg2d.timegrid[-1], y2[1], 'x', c='silver', markersize=10)
+plt.plot(bridg2d.timegrid[0], y1[1], '.g')
 plt.suptitle('Zoomed in each dimension seperately')
 
 
