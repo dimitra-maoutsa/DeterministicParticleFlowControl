84a10923d6e5fcc00843099bf810ad2b14b7ac67
Dimitra Maoutsa <maoutsa.d@hotmail.gr>
2021-12-28 18:54:50 +0100

    Update __init__.py

--- DeterministicParticleFlowControl/__init__.py
+++ DeterministicParticleFlowControl/__init__.py
@@ -9,5 +9,8 @@ Created on Sat Dec 11 23:49:44 2021
 
 
 from __future__ import absolute_import, division, print_function
+# For relative imports to work in Python 3.6
+import os, sys
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 from .version import __version__  
 from .DeterministicParticleFlowControl import *  
