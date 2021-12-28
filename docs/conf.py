2dbbb5cbf9a39f5a7f55d39098562488dfad0793
Dimitra Maoutsa <maoutsa.d@hotmail.gr>
2021-12-28 21:22:38 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -114,7 +114,7 @@ sphinx_gallery_conf = {
     # path where to save gallery generated examples
     'gallery_dirs': 'auto_examples',
     # To auto-generate example sections in the API
-    'doc_module': ('DeterministicParticleFlowControl',),
+    'doc_module': ('DeterministicParticleFlowControl', 'score_estimators'),
     # Auto-generated mini-galleries go here
     'backreferences_dir': 'gen_api'
 }
