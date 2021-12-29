e2bb1b4e5e4354efd744d7c296b496500bd3ca6f
Dimitra Maoutsa <maoutsa.d@hotmail.gr>
2021-12-29 19:35:35 +0100

    Update DeterministicParticleFlowControl.py

--- DeterministicParticleFlowControl/DeterministicParticleFlowControl.py
+++ DeterministicParticleFlowControl/DeterministicParticleFlowControl.py
@@ -15,8 +15,7 @@ import numpy as np
 #import ot
 #import numba
 from matplotlib import pyplot as plt
-#import score_estimators
-#import reweighting
+
 if __name__ == "DeterministicParticleFlowControl.DeterministicParticleFlowControl":
     from .score_estimators.score_function_estimators import  score_function_multid_seperate
     from .reweighting.optimal_transport_reweighting import reweight_optimal_transport_multidim
