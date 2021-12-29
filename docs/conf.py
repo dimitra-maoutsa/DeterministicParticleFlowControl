e9a131dfd90fe709002a4dadbffa1bf7b99cd41c
Dimitra Maoutsa <maoutsa.d@hotmail.gr>
2021-12-29 02:29:07 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -83,7 +83,7 @@ extensions = ['nbsphinx','sphinx.ext.autodoc',
 # Napoleon settings
 napoleon_google_docstring = True
 napoleon_numpy_docstring = True
-napoleon_include_init_with_doc = False
+napoleon_include_init_with_doc = True
 napoleon_include_private_with_doc = False
 napoleon_include_special_with_doc = True
 napoleon_use_admonition_for_examples = False
